1e598861c4df1ff67ccc2b60ddf42b97934b99b8
mike <mike@pop-os.localdomain>
2023-06-17 17:07:47 -0700

    various bugfixes, optimization of dust temperature solver

--- ismulator.py
+++ ismulator.py
@@ -1,11 +1,8 @@
 import streamlit as st
-#import SessionState
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 from ism_eqm_temp import *
-
-#session_initial_state = SessionState.get()
 
 NH2 = 10**st.sidebar.slider(r'Column density: $\log N_{\rm H}/\rm cm^{-2}$ at $n_{\rm H}=100\,\rm cm^{-3}$',min_value=19.,max_value=23., value=21.)
 Z = 10**st.sidebar.slider(r'Metallicity: $\log Z$',min_value=-4.,max_value=1., value=0.)
@@ -15,22 +12,36 @@ ISRF = 10**st.sidebar.slider(r'$\log \chi$: Interstellar radiation field strengt
 NH_alpha = st.sidebar.slider(r'Column density scaling: $\alpha$ where $N_{\rm H}=N_{\rm H,0}\left(n_{\rm H}/100\rm cm^{-3}\right)^{\alpha}$',min_value=0.,max_value=1., value=.3)
 fJ = st.sidebar.slider(r'$f_{\rm J}=l/\lambda_{\rm J}$: Ratio of shielding length floor to Jeans wavelength',min_value=0.,max_value=1.,value=0.25)
 dust_beta = st.sidebar.slider(r'$\beta$: Dust spectral index',min_value=1.,max_value=2.,value=2.)
-compression = st.sidebar.checkbox(r"Gravitational collapse heating",value=True)
-attenuate_cr = st.sidebar.checkbox(r"Cosmic ray attenuation",value=True)
+compression = st.checkbox(r"Gravitational collapse heating",value=True)
+attenuate_cr = st.checkbox(r"Cosmic ray attenuation",value=True)
+jeans_mass = st.checkbox(r"Plot Jeans mass",value=False)
 CO_Rx = st.sidebar.selectbox("CO cooling prescription", ["Whitworth 2018","Gong 2017","Hopkins 2022 (FIRE-3)"])
 CI_Rx = st.sidebar.selectbox("Atomic cooling prescription", ["Hopkins 2022 (FIRE-3)","Simple"])
 
-#reset = st.button("Reset")
-#if reset:
-    #session_state = session_initial_state
+# sliders for plot limits
+Tmin = 1
+Tmax = 1e4
+nmin = 1
+nmax = 1e12
 
-n = np.logspace(0,13,100)
+#switches for different cooling physics
+
+
+n = np.logspace(0,16,100)
 NH = NH2 * (n/1e2)**NH_alpha
 T, Tdust = equilibrium_temp(n,NH,jeans_shielding=fJ, compression=compression,Z=Z,z=z,ISRF=ISRF,zeta_CR=zeta_CR,
 attenuate_cr=attenuate_cr,return_Tdust=True,dust_beta=dust_beta,co_prescription=CO_Rx,cii_prescription=CI_Rx)
+
 fig,ax = plt.subplots()
-ax.loglog(n,T,label="Gas",color='black')
-ax.loglog(n,Tdust,label="Dust",color='black',ls='dashed')
-ax.legend(loc=1)
-ax.set(xlim=[1,1e13],ylim=[1,1e4],xlabel=r"$n_{\rm H}\,\left(\rm cm^{-3}\right)$",ylabel=r"$T\left(\rm K\right)$")
-st.write(fig)
+if jeans_mass:
+    MJ = 5 * (T/10)**1.5 * (n/1e4)**-0.5
+    ax.loglog(n,MJ,color='black')
+    ax.set(xlim=[1,1e16],ylim=[1e-4,1e4],xlabel=r"$n_{\rm H}\,\left(\rm cm^{-3}\right)$",ylabel=r"$M_{\rm J}\left(M_\odot\right)$")
+else:
+    ax.loglog(n,T,label="Gas",color='black')
+    ax.loglog(n,Tdust,label="Dust",color='black',ls='dashed')
+    ax.legend(loc=1)
+    ax.set(xlim=[nmin,nmax],ylim=[Tmin,Tmax],xlabel=r"$n_{\rm H}\,\left(\rm cm^{-3}\right)$",ylabel=r"$T\left(\rm K\right)$")
+st.pyplot(fig)
+
+    
